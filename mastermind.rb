a221076b70255e3c33c833340b420b9416967d56
Sherry He <xsherryhe@gmail.com>
2022-01-22 20:44:47 -0500

    Added feedback method for codemaker and history display method

--- mastermind.rb
+++ mastermind.rb
@@ -37,12 +37,29 @@ module Mastermind
       puts Colors
       @codebreaker = Codebreaker.new
       @codemaker = Codemaker.new
-      @history = []
+      @history = {}
+      @history.compare_by_identity
     end
 
     def play_round
-      @codebreaker.make_guess
-      p @codebreaker.guess
+      guess = @codebreaker.guess
+      feedback = @codemaker.feedback(guess)
+      @history[guess] = feedback
+      display_history
+    end
+
+    private
+
+    def display_history
+      puts guesses_so_far
+      @history.each_with_index do |(guess, feedback), i|
+        puts "#{' ' * 5}Guess \##{i + 1}. #{guess.join}"
+        puts feedback.split("\r\n").map { |line| line.prepend(' ' * 10) }.join("\r\n")
+      end
+    end
+
+    def guesses_so_far
+      "Guesses so far: #{@history.size}/12"
     end
   end
 
@@ -54,43 +71,62 @@ module Mastermind
       @code = 4.times.map { letters.sample }
       puts 'The computer has chosen a code with four peg colors.'
     end
+
+    def feedback(guess)
+      @correct_color_position = 0
+      @correct_color = 0
+      letters.each { |letter| give_letter_feedback(letter, guess) }
+      "Pegs with correct color and position: #{@correct_color_position}\r\n" \
+      "Pegs with correct color only: #{@correct_color}"
+    end
+
+    private
+
+    def give_letter_feedback(letter, guess)
+      letter_correct_color_position =
+        guess.each_with_index
+             .count { |guess_letter, i| guess_letter == code[i] && guess_letter == letter }
+      @correct_color_position += letter_correct_color_position
+      @correct_color += [guess.count(letter), code.count(letter)].min - letter_correct_color_position
+    end
   end
 
   class Codebreaker
     include Colors
-    attr_reader :name, :guess
+    attr_reader :name
 
     def initialize
       puts 'Codebreaker, what is your name?'
       @name = gets.chomp
     end
 
-    def make_guess
+    def guess
       puts "Guess the code, #{name}! Type four letters for four colors (e.g., ROYG)."
       puts Colors.shorthand
-      @guess = gets.chomp.upcase
+      @guess = gets.chomp.upcase.split('')
       validate_guess
+      @guess
     end
 
     private
 
     def valid_letters_only
-      @guess.chars.all? { |char| letters.join.include?(char) }
+      @guess.all? { |letter| letters.join.include?(letter) }
     end
 
     def validate_guess
-      until @guess.length == 4 && valid_letters_only
-        puts 'Please type FOUR letters (e.g., ROYG).' unless guess.length == 4
+      until @guess.size == 4 && valid_letters_only
+        puts 'Please type FOUR letters (e.g., ROYG).' unless @guess.size == 4
         puts 'Please only type letters corresponding to valid colors.' unless valid_letters_only
-        @guess = gets.chomp.upcase
+        @guess = gets.chomp.upcase.split('')
       end
     end
   end
 end
 
 game = Mastermind::Game.new
-game.play_round
+12.times { game.play_round }
 
-# TODO: codemaker method to give feedback using pegs)
 # TODO: game over conditions
+# TODO: customizable total guesses allowed and code length
 # TODO: make human and computer subclasses for codemaker and codebreaker once needed
